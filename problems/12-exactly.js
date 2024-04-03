fc52ad9d9e54a622001cb5709ce6f9d7d242fe99
DoctorDoofen <and.bennett0930@gmail.com>
2024-04-02 22:20:46 -0500

    12 solved

--- problems/12-exactly.js
+++ problems/12-exactly.js
@@ -26,14 +26,46 @@ let result4 = exactly(['follow', 'the', 'yellow', 'brick', 'road'], 0, function 
 console.log(result4); // true
 
 *******************************************************************************/
+// Write a function `exactly` that accepts an array, a number, and a callback as
+// arguments. The function should return a boolean indicating whether or not there are
+// exactly `number` elements of the array that return true when passed into the callback.
+
 
 function exactly(array, num, cb) {
-  // Your code here 
+  let count = 0;
+  for (let el of array) {
+    if (cb(el) === true) {
+      count++;
+    }
+  }
+  if (count === num) {
+    return true;
+  }
+  return false;
 }
+let result1 = exactly([18, 5, 32, 7, 100], 3, function (n) {
+  return n % 2 === 0;
+});
+console.log(result1); // true
+
+let result2 = exactly([18, 5, 32, 7, 100], 2, function (n) {
+  return n % 2 === 0;
+});
+console.log(result2); // false
+
+let result3 = exactly(['follow', 'the', 'yellow', 'brick', 'road'], 1, function (str) {
+  return str.includes('x');
+});
+console.log(result3); // false
+
+let result4 = exactly(['follow', 'the', 'yellow', 'brick', 'road'], 0, function (str) {
+  return str.includes('x');
+});
+console.log(result4); // true
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = exactly;
-} catch(e) {
+} catch (e) {
   return null;
 }
