1f232045fb5d0a92eca4d3a8d728df859372555f
DoctorDoofen <and.bennett0930@gmail.com>
2024-04-02 21:39:26 -0500

    08 solved

--- problems/08-count.js
+++ problems/08-count.js
@@ -25,14 +25,41 @@ let result4 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str)
 });
 console.log(result4); // 0
 *******************************************************************************/
-
+// Write a function `count` that accepts an array and a callback as arguments. The
+// function should return the number of elements of the array that return true when
+// passed to the callback.
 function count(array, cb) {
-  // Your code here 
+  let count = 0;
+  for (let el of array) {
+    if (cb(el) === true) {
+      count++;
+    }
+  }
+  return count;
 }
 
+let result1 = count([18, 5, 32, 7, 100], function (n) {
+  return n % 2 === 0;
+});
+console.log(result1); // 3
+
+let result2 = count([17, 5, 31, 7, 100], function (n) {
+  return n % 2 === 0;
+});
+console.log(result2); // 1
+
+let result3 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str) {
+  return str.includes('o');
+});
+console.log(result3); // 3
+
+let result4 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str) {
+  return str.includes('x');
+});
+console.log(result4); // 0
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = count;
-} catch(e) {
+} catch (e) {
   return null;
 }
